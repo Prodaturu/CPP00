43830cbb21c27b9c0c7a97cdfaf4e4e47d841e36
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-08-18 15:13:23 +0200

    test files added. NOTE: should be excluded from the push to school's remote

--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 09:10:58 by sprodatu          #+#    #+#             */
+/*   Updated: 2024/08/18 09:31:43 by sprodatu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #ifndef PHONEBOOK_HPP
@@ -5,13 +17,17 @@
 
 #include<iostream>
 
+// first name, last name, nickname, phone number, and
+// darkest secret. A saved contact can’t have empty fields.
+
 class PhoneBook
 {
 	private:
-		
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;	
 }
 
 #endif
-
-first name, last name, nickname, phone number, and
-darkest secret. A saved contact can’t have empty fields.
